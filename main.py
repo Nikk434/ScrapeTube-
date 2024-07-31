e6060db8e782e77aa6d2e24d7d38d85f2a656031
Nikk434 <nskshirsagar434@gmail.com>
2024-07-31 09:45:32 +0530

    main script

---
+++ main.py
@@ -0,0 +1,55 @@
+from pytube import Playlist
+import ffmpeg
+import os
+import re
+
+def sanitize_filename(filename):
+    return re.sub(r'[<>:"/\\|?*]', '', filename)
+
+def download_youtube_playlist(playlist_url, download_path):
+    playlist = Playlist(playlist_url)
+    
+    print(f'Downloading playlist: {playlist.title}')
+    
+    for video in playlist.videos:
+        sanitized_title = sanitize_filename(video.title)
+        
+        final_path = os.path.join(download_path, f'{sanitized_title}.mp4')
+        
+        if os.path.exists(final_path):
+            print(f'Skipping already downloaded video: {sanitized_title}')
+            continue
+        
+        print(f'Downloading video: {sanitized_title}')
+        
+        video_stream = video.streams.filter(res="1080p").first()
+        
+        audio_stream = video.streams.filter(only_audio=True, mime_type="audio/mp4").order_by("abr").desc().first()
+        
+        video_path = os.path.join(download_path, f'{sanitized_title}_video.mp4')
+        audio_path = os.path.join(download_path, f'{sanitized_title}_audio.mp4')
+        
+        if video_stream:
+            video_stream.download(output_path=download_path, filename=f'{sanitized_title}_video.mp4')
+        if audio_stream:
+            audio_stream.download(output_path=download_path, filename=f'{sanitized_title}_audio.mp4')
+        
+        if video_stream and audio_stream:
+            video_input = ffmpeg.input(video_path)
+            audio_input = ffmpeg.input(audio_path)
+            
+            ffmpeg.output(video_input, audio_input, final_path, vcodec='copy', acodec='aac', strict='experimental').run(overwrite_output=True)
+        
+            os.remove(video_path)
+            os.remove(audio_path)
+        
+        print(f'Video downloaded and merged: {sanitized_title}')
+
+    print("Download completed.")
+
+playlist_url = '' #paste your Playlist url
+
+download_path = '' #your download path
+
+download_youtube_playlist(playlist_url, download_path)
+
